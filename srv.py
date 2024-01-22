2320864aaa41441118d5437e5ee51b67340eac6c
Angel Mateo Gonzalez <angegonzalez@unal.edu.co>
2024-01-22 15:43:29 +0100

    Refactor and changing approach of showing image solving merge problems

--- srv.py
+++ srv.py
@@ -2,9 +2,8 @@ from concurrent import futures
 import grpc
 import file_pb2
 import file_pb2_grpc
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import io
+from PIL import Image
+from io import BytesIO
 
 class MonServiceServicer(file_pb2_grpc.MonServiceServicer):
     def DireBonjour(self, request, context):
@@ -21,23 +20,21 @@ class MonServiceServicer(file_pb2_grpc.MonServiceServicer):
 
         # Renvoyer un message de remerciement dans la réponse
         return file_pb2.ReponseCoordonnees(message="Merci pour les coordonnées!")
-    
+
     def UploadImage(self, request_iterator, context):
+        image_data = b''
         for chunk in request_iterator:
-            # Traitez chaque morceau d'image ici (par exemple, affichez-le ou enregistrez-le)
-            image_data = chunk.data
-            image_array = bytearray(image_data)
-
-            # Utilisez Matplotlib pour afficher l'image
-            image = mpimg.imread(io.BytesIO(image_array), format='JPG')
-            plt.imshow(image)
-            plt.show()
-
-        # Vous pouvez renvoyer une réponse indiquant le succès de l'opération
-        return streaming_pb2.UploadStatus(success=True)
+            image_data += chunk.data
+        image = Image.open(BytesIO(image_data))
+        image.show()
+        return file_pb2.UploadStatus(success=True)
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    MAX_MESSAGE_LENGTH = 100 * 1024 * 1024
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), options = [
+        ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
+        ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
+    ])
     file_pb2_grpc.add_MonServiceServicer_to_server(MonServiceServicer(), server)
     
     # Modifiez cette ligne pour écouter sur toutes les interfaces réseau
